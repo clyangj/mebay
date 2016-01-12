e07afb83e5d01d98a4bc0be3f9037d88e3e2a0fc
yangjian <jian.yang@dongbadongba.com>
2016-01-12 15:42:00 +0800

    params permit

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -4,22 +4,48 @@ class AdsController < ApplicationController
     @ad = Ad.find params[:id]
   end
 
+  ##
+  # ==== Description
+  #   ad列表
+  #   ads Get /ads(.:format)
+  #
   def index
     @ads = Ad.all
   end
 
+  ##
+  # ==== Description
+  #   创建ad
+  #   new_ad Get /ads/new(.:format)
+  #
   def new
     @ad = Ad.new
   end
 
+  ##
+  # ==== Description
+  #   创建ad
+  #   ads Post /ads(.:format)
+  #
   def create
-    ad = Ad.create params[:ad]
+    ad = Ad.create ad_pramas
     if ad.save
       flash[:notice] = '保存成功'
-      redirect_io new_ad_path
+      redirect_to ads_path
     else
       flash[:notice] = ad.errors.full_messages.join("\n")
       render :new
     end
+  end
+
+  ##
+  # ==== Description
+  #   过滤数据
+  #
+  def ad_pramas
+    params.require(:ad).permit(
+      :name, :description, :price, :seller_id, 
+      :email, :img_url_string
+    )
   end
 end
