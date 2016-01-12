b3ac3452271c550c65d5d0b5f26533c3d9081880
yangjian <jian.yang@dongbadongba.com>
2016-01-12 19:10:11 +0800

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
   get 'static_pages/home'
 
   get 'static_pages/help'
 
+  get 'static_pages/about'
   resources :microposts
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
