4639992682bb91e38409aa867060552116078c54
yangjian <jian.yang@dongbadongba.com>
2016-01-18 11:43:13 +0800

    full_title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  ##
+  # ==== Description
+  #   设置消息标题
+  #
+  def full_title(page_title = '')
+    base_title = 'Ruby On Rails'
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
